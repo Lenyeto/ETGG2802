39fc7abdae2b5ac2eeeb19982c812a73d54128f2
JalenTheEpic <FearTheDurp@gmail.com>
2016-02-04 10:07:41 -0500

    ADDED HIT SHIT AGAIN
    JESUS

--- src/Physics/HitBox.java
+++ src/Physics/HitBox.java
@@ -21,5 +21,10 @@ public class HitBox implements HitData {
     public boolean CheckCollision(HitBox box) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
+
+    @Override
+    public void draw() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
--- src/Physics/HitData.java
+++ src/Physics/HitData.java
@@ -11,5 +11,5 @@ public interface HitData {
      boolean CheckCollision(HitSphere sp);
      boolean CheckCollision(HitPlane pl);
      boolean CheckCollision(HitBox box);
-    
+     void draw();
 }
