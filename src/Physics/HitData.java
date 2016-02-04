752090dd154d5343f186a12e836f69e5b49befb5
JalenTheEpic <FearTheDurp@gmail.com>
2016-02-04 09:55:41 -0500

    Added hit-checking classes and a utility package

--- src/Physics/HitData.java
+++ src/Physics/HitData.java
@@ -11,5 +11,6 @@ public interface HitData {
      boolean CheckCollision(HitSphere sp);
      boolean CheckCollision(HitPlane pl);
      boolean CheckCollision(HitBox box);
+     void draw();
     
 }
--- src/Physics/HitPlane.java
+++ src/Physics/HitPlane.java
@@ -20,6 +20,11 @@ public class HitPlane implements HitData {
     public boolean CheckCollision(HitBox box) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
+
+    @Override
+    public void draw() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
     
     
